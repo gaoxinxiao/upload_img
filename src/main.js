72badc2996c9d43dab338a19ebdb0ce9634b960f
gaoxinxiao <18210833386@163.com>
2021-03-10 18:49:31 +0800

    性能监控部署

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import routes from '@/routes/routes'
+window.localStorage.wmUserInfo = JSON.stringify({ userId: '18210833386', userTag: '蛤蟆金服', projectVersion: '1.0.1' })
 
 createApp(App)
     .use(routes)
