f3009b546ad099b74fbd0a2a34384ab3eb9a7d98
gaoxinxiao <18210833386@163.com>
2021-03-25 18:52:15 -0700

    11

--- src/views/wmh.vue
+++ src/views/wmh.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="wrap">
     <ul
-      v-for="(v, index) in listWrap"
+      v-for="(pitem, index) in dataList"
       :key="index"
       :class="`list ${selectIndex === index ? 'list_hei' : ''}`"
     >
       <li
         @click="open(index)"
-        v-for="(item, ind) in list"
+        v-for="(item, ind) in pitem.details"
         :key="ind"
-        :style="`top:${
-          selectIndex === index ? (100 + 10) * item : item * 10
-        }px`"
+        :style="`
+        top:${
+          selectIndex === index
+            ? (100 + 10) * item.lastIndex
+            : item.lastIndex * 10
+        }px; 
+        width:${selectIndex === index ? null : 300 - item.lastIndex * 20}px
+        `"
       >
-        {{ item }}
+        {{ item.sendNickName }}
       </li>
     </ul>
   </div>
@@ -22,23 +27,156 @@
 export default {
   data() {
     return {
-      list: [2, 1, 0],
-      listWrap: [1, 2,3],
+      dataList: [],
       selectIndex: null,
       flag: false,
       flag1: false,
     };
   },
+  mounted() {
+    let internaleNotic = [
+      {
+        sourceId: "OKR",
+        noticeNum: 3,
+        createType: "1",
+        details: [
+          {
+            sendNickName: "刘恺丽",
+            releaseTime: "2021年03月22日 14时33分发布",
+            sourceBusinessId: "20210322141300258",
+            isIgnore: false,
+            typeName: "OKR被@提醒",
+            contentId: 7,
+            msgId: 7,
+            msgDetail: null,
+            receiveUserName: "wumeihong-jk",
+            receiveId: 8,
+            receiveTime: "2021-03-22 14:33:52",
+            receiveNickName: "吴美红",
+            msgContent: null,
+            msgSubject: "OKR被@提醒",
+            msgTitle: "周期[2021年1季度]",
+            msgName: "周期[2021年1季度]",
+            sendUserName: "liukaili1-jk",
+            typeId: "OKRATRemind",
+          },
+          {
+            sendNickName: "刘恺丽",
+            releaseTime: "2021年03月22日 14时31分发布",
+            sourceBusinessId: "20210322141300257",
+            isIgnore: false,
+            typeName: "被对齐提醒",
+            contentId: 6,
+            msgId: 6,
+            msgDetail: null,
+            receiveUserName: "wumeihong-jk",
+            receiveId: 7,
+            receiveTime: "2021-03-22 14:31:48",
+            receiveNickName: "吴美红",
+            msgContent: null,
+            msgSubject: "被对齐提醒",
+            msgTitle: "周期[2021年1季度]",
+            msgName: "周期[2021年1季度]",
+            sendUserName: "liukaili1-jk",
+            typeId: "OKRAlignRemind",
+          },
+          {
+            sendNickName: "系统通知",
+            releaseTime: "2021年01月01日 14时28分发布",
+            sourceBusinessId: "20210322141300253",
+            isIgnore: false,
+            typeName: "周期开始填报OKR",
+            contentId: 5,
+            msgId: 5,
+            msgDetail: null,
+            receiveUserName: "wumeihong-jk",
+            receiveId: 6,
+            receiveTime: "2021-01-01 14:28:49",
+            receiveNickName: "吴美红",
+            msgContent: null,
+            msgSubject: "周期开始填报OKR",
+            msgTitle: "周期[2021年1季度]",
+            msgName: "周期[2021年1季度]",
+            sendUserName: "admin",
+            typeId: "OKRPeriodFillIn",
+          },
+        ],
+        sourceName: "OKR系统",
+      },
+      {
+        sourceId: "week",
+        noticeNum: 3,
+        createType: "1",
+        details: [
+          {
+            sendNickName: "刘恺丽",
+            releaseTime: "2021年03月22日 14时33分发布",
+            sourceBusinessId: "20210322141300258",
+            isIgnore: false,
+            typeName: "OKR被@提醒",
+            contentId: 7,
+            msgId: 7,
+            msgDetail: null,
+            receiveUserName: "wumeihong-jk",
+            receiveId: 8,
+            receiveTime: "2021-03-22 14:33:52",
+            receiveNickName: "吴美红",
+            msgContent: null,
+            msgSubject: "OKR被@提醒",
+            msgTitle: "周期[2021年1季度]",
+            msgName: "周期[2021年1季度]",
+            sendUserName: "liukaili1-jk",
+            typeId: "OKRATRemind",
+          },
+          {
+            sendNickName: "刘恺丽",
+            releaseTime: "2021年03月22日 14时31分发布",
+            sourceBusinessId: "20210322141300257",
+            isIgnore: false,
+            typeName: "被对齐提醒",
+            contentId: 6,
+            msgId: 6,
+            msgDetail: null,
+            receiveUserName: "wumeihong-jk",
+            receiveId: 7,
+            receiveTime: "2021-03-22 14:31:48",
+            receiveNickName: "吴美红",
+            msgContent: null,
+            msgSubject: "被对齐提醒",
+            msgTitle: "周期[2021年1季度]",
+            msgName: "周期[2021年1季度]",
+            sendUserName: "liukaili1-jk",
+            typeId: "OKRAlignRemind",
+          },
+        ],
+        sourceName: "OKR系统",
+      },
+    ];
+
+    internaleNotic = internaleNotic.map((item) => {
+      return {
+        ...item,
+        details: this.formtData(item.details),
+      };
+    });
+    console.log(internaleNotic)
+    this.dataList = internaleNotic;
+  },
   methods: {
+    formtData(list) {
+      return list.map((res, ind) => {
+        return {
+          lastIndex: list.length - (ind + 1),
+          ...res,
+        };
+      });
+    },
     open(index) {
       if (this.selectIndex === index) {
         this.selectIndex = null;
         return;
       }
       this.selectIndex = index;
-    },
-    open1() {
-      this.flag1 = !this.flag1;
     },
   },
 };
@@ -59,6 +197,9 @@ export default {
   width: 300px;
   height: 120px;
   transition: 0.3s;
+  display: flex;
+  justify-content: center;
+  margin-top:20px;
 }
 .list li {
   width: 300px;
@@ -70,8 +211,9 @@ export default {
   position: absolute;
   cursor: pointer;
   transition: 0.3s;
+  background: #ccc;
 }
-.list li:first-child {
+/* .list li:first-child {
   background: red;
 }
 .list li:nth-child(2) {
@@ -79,33 +221,5 @@ export default {
 }
 .list li:nth-child(3) {
   background: green;
-}
-.list1 {
-  position: relative;
-  width: 300px;
-  height: 120px;
-  margin-top: 10px;
-}
-
-.list1 li {
-  width: 300px;
-  height: 100px;
-  text-align: center;
-  line-height: 100px;
-  border-radius: 15px;
-  margin-top: 10px;
-  position: absolute;
-  cursor: pointer;
-  transition: 0.3s;
-}
-
-.list1 li:first-child {
-  background: red;
-}
-.list1 li:nth-child(2) {
-  background: yellow;
-}
-.list1 li:nth-child(3) {
-  background: green;
-}
+} */
 </style>
